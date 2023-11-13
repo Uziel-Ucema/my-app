26643884227bf186c9d2efca0a075b8cee36d0a8
Uziel Luna <uzielluna24@gmail.com>
2023-11-13 12:24:35 -0300

    Alerts Changes

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,4 +1,12 @@
 class AlertsController < ApplicationController
+    before_action :authenticate_user!
     def index
+        @folders = Folder.all
+    end
+    
+    # Other actions...
+    
+    def save_changes
+        # Handle the logic to save changes here
     end
 end
