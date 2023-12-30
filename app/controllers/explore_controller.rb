c763c00f81232498a09a86108b73b80e265ec76b
Uziel Luna <uzielluna24@gmail.com>
2023-12-30 16:25:57 -0300

    Explore modifications

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,4 +1,5 @@
 class ExploreController < ApplicationController
     def index
+        @dog_breeds = Dir.glob('app/assets/images/DogFrames/*').map { |folder| File.basename(folder) }
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'contact', to: 'contact#new'
   post 'contact', to: 'contact#create'
-  get "explore", to: "explore#index"
+  get 'explore', to: 'explore#index'
   get "alerts", to: "alerts#index"
   post '/save_changes', to: 'alerts#save_changes'
 
