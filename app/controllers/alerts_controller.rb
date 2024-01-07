e26c1716eccd344d5f59c8862871372e899f95d8
Uziel Luna <uzielluna24@gmail.com>
2024-01-07 09:16:41 -0300

    Small alert fix 0701

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -6,10 +6,18 @@ class AlertsController < ApplicationController
         
     def save_changes
         selected_folder_ids = params[:folders]
-        current_user.folders = Folder.find(selected_folder_ids)
-        # Update the associations based on the selected_folder_ids
+    
+        if selected_folder_ids.present?
+          current_user.folders = Folder.find(selected_folder_ids)
+          # Update the associations based on the selected_folder_ids
+        else
+          # Delete all folders_users relations when no folder is selected
+          current_user.folders = []  # Clear the associations
+          FoldersUser.where(user_id: current_user.id).delete_all
+          flash[:notice] = 'All folder-user relations deleted.'
+        end
     
         # Assuming you have a redirect or render logic
         redirect_to root_path, notice: 'Changes saved successfully!'
-    end
+      end
 end
